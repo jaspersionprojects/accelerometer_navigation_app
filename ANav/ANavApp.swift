10e20727ad5a3137fc29e41a0bdfe44ce33a0444
Jasper Sion <mail@jaspersion.com>
2026-03-17 00:04:36 +0000

    Initial Commit

---
+++ ANav/ANavApp.swift
@@ -0,0 +1,17 @@
+//
+//  ANavApp.swift
+//  ANav
+//
+//  Created by Jasper Sion on 17/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct ANavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ANav/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ANav
+//
+//  Created by Jasper Sion on 17/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
